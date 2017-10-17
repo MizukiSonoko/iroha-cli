919d67e6f46d4f991cc5caa5893beebfe94e0d9e
mizukisonoko <mizuki.sonoko@gmail.com>
2017-10-17 12:44:50 +0300

    Add hash mock

--- cli/crypto.py
+++ cli/crypto.py
@@ -6,3 +6,6 @@ def generate_hex_sstr():
     publicKey64 = "Not implemente"
     privateKey64 = "Not implemente"
     return (publicKey64,privateKey64)
+
+def hash(msg):
+    return "Not implemente"
