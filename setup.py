56ca21362f4d9b271c0c27d13dba4afa6a4d595f
mizukisonoko <mizuki.sonoko@gmail.com>
2017-10-27 17:28:20 +0300

    Update version 1.2.1 -> 1.2.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class GeneratePb(_build_py):
 if __name__ == '__main__':
     setup(
           name='iroha-ya-cli',
-          version='1.2.1',
+          version='1.2.2',
           description='Cli for hyperledger/iroha',
           author='Sonoko Mizuki',
           license='Apache',
