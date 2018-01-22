a9259c80ebc169f7f20ab9e59e888e24bdd63d9b
mizukisonoko <mizuki.sonoko@gmail.com>
2018-01-22 14:53:37 +0900

    Add ed25519 to requirements

--- iroha_cli/crypto.py
+++ iroha_cli/crypto.py
@@ -6,29 +6,14 @@ import os
 from collections import namedtuple
 
 
-class KeyPair(namedtuple('KeyPair', ['raw_private_key', 'raw_public_key'])):
-    def encode(value):
-        return base64.b64encode(value)
-
-    def __encode__(self, value):
-        return base64.b64encode(value)
-
-    def decode(value):
-        return base64.b64decode(value)
-
-    def __decode__(self,value):
-        return base64.b64decode(value)
-
-    @property
-    def private_key(self):
-        return self.__encode__(self.raw_private_key)
-
-    @property
-    def public_key(self):
-        return self.__encode__(self.raw_public_key)
+class KeyPair:
+    def __init__(self, pub, pri):
+        self.private_key = pri
+        self.public_key = pub
 
 
-from iroha_cli.crypto_ed25519 import generate_keypair_ed25519, sign_ed25519, verify_ed25519
+from iroha_cli.crypto_ed25519 import generate_keypair_ed25519, sign_ed25519, verify_ed25519, ed25519_sha3_512, \
+    ed25519_sha3_256
 
 
 def generate_keypair():
@@ -36,7 +21,7 @@ def generate_keypair():
 
 
 def sign(key_pair, message):
-    return base64.b64decode(sign_ed25519(key_pair, message))
+    return sign_ed25519(key_pair, message)
 
 
 def verify(pub_key, sig, message):
@@ -44,4 +29,8 @@ def verify(pub_key, sig, message):
 
 
 def sha3_256(message):
-    return sha3.sha3_256(message).digest()
+    return ed25519_sha3_256(message)
+
+
+def sha3_512(message):
+    return ed25519_sha3_512(message)
