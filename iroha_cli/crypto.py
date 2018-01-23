666b011ef95ef6e82e59cc134b52fb29443ff9d8
mizukisonoko <mizuki.sonoko@gmail.com>
2018-01-23 16:28:35 +0900

    Add get raw key from KeyPair

--- iroha_cli/crypto.py
+++ iroha_cli/crypto.py
@@ -11,6 +11,8 @@ class KeyPair:
         self.private_key = pri
         self.public_key = pub
 
+    def raw_public_key(self):
+        return base64.b64decode(self.public_key)
 
 from iroha_cli.crypto_ed25519 import generate_keypair_ed25519, sign_ed25519, verify_ed25519, ed25519_sha3_512, \
     ed25519_sha3_256
